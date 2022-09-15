b3a137c8720f68c3666d85e258a0e93ba769a29f
dylan <dylangloignacio@gmail.com>
2022-09-15 10:35:02 -0700

    first commit

---
+++ gitexercise/src/gitexercise/ApplicationMain.java
@@ -0,0 +1,22 @@
+package gitexercise;
+
+public class ApplicationMain {
+	
+	public static void main(String[] args) {
+		String[] shopList = new String[] {
+				"Apples",
+				"Bananas",
+				"Corn",
+				"Dog Food",
+				"Cereal", 
+				"Bread", 
+				"Eggs",
+				"Milk"
+		};
+		
+		for (String item: shopList) {
+			System.out.println(item);
+		}	
+       }
+ 
+}
